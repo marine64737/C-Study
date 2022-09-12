0792c6e1c96f18270148b66d9c4bb4803c1d11d7
marine64737 <81956909+marine64737@users.noreply.github.com>
2022-09-12 21:00:11 +0900

    Update ex.c

--- C Study/C Study/ex.c
+++ C Study/C Study/ex.c
@@ -8,8 +8,21 @@ void RemoveBSN(char str[])
 	str[len - 1] = 0;
 }
 int main(void) {
-	char str[20] ;
-	fgets(str, sizeof(str), stdin);
+	char str1[20] ;
+	char str2[20] ;
+	int SP1 = 0;
+	int SP2 = 0;
+
+	fgets(str1, sizeof(str1), stdin);
+	fgets(str2, sizeof(str2), stdin);
+
+	RemoveBSN(str1);
+	RemoveBSN(str2);
+
+	for (int i = 0; i < strlen(str1); i++)
+		if (str1[i] == ' ') SP1 = i;
+	for (int i = 0; i < strlen(str2); i++)
+		if (str2[i] == ' ') SP2 = i;
 
 	return 0;
 }
