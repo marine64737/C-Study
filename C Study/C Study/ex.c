6e28e78ffa6a516c6fe52e99eabe6317fd90aa2d
marine64737 <81956909+marine64737@users.noreply.github.com>
2022-09-25 14:50:51 +0900

    Update ex.c

--- C Study/C Study/ex.c
+++ C Study/C Study/ex.c
@@ -7,14 +7,24 @@ void RemoveBSN(char str[])
 	int len = strlen(str);
 	str[len - 1] = 0;
 }
+int GetSpace(char str[])
+{
+	for (int i = 0; i < strlen(str); i++)
+		if (str[i] == ' ') return 1;
+	return 0;
+}
 int main(void) {
-	char str1[20] ;
-	char str2[20] ;
+	char str1[20];
+	char str2[20];
 	int SP1 = 0;
 	int SP2 = 0;
 
-	fgets(str1, sizeof(str1), stdin);
-	fgets(str2, sizeof(str2), stdin);
+	while (1) {
+		fgets(str1, sizeof(str1), stdin);
+		fgets(str2, sizeof(str2), stdin);
+		if (!GetSpace(str1) || !GetSpace(str2)) break;
+		else printf("Please retry!");
+	}
 
 	RemoveBSN(str1);
 	RemoveBSN(str2);
@@ -23,6 +33,7 @@ int main(void) {
 		if (str1[i] == ' ') SP1 = i;
 	for (int i = 0; i < strlen(str2); i++)
 		if (str2[i] == ' ') SP2 = i;
+	if(SP1 != SP2)
 
 	return 0;
 }
