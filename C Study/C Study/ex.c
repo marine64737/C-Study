f7af61dc1ea804e8f7303f4819a8cee4352de9f6
marine64737 <81956909+marine64737@users.noreply.github.com>
2022-09-28 23:07:45 +0900

    Update ex.c

--- C Study/C Study/ex.c
+++ C Study/C Study/ex.c
@@ -17,4 +17,10 @@ int main(void)
 	
 	fputs("point2 pos: ", stdout);
 	scanf("%d %d", &pos2.xpos, &pos2.ypos);
+
+	distance = sqrt((double)((pos1.xpos - pos2.xpos) * (pos1.xpos - pos2.xpos)) +
+		(double)((pos1.ypos - pos2.ypos) * (pos1.ypos - pos2.ypos)));
+	printf("두 점의 거리는 %g입니다. \n", distance);
+
+	return 0;
 }
